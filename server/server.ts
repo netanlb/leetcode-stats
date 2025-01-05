a031371dc73f7e7f667bf9831904d7d52dab6f63
netanlb <netanlb@gmail.com>
2025-01-05 14:33:05 +0200

    deploy

--- server/server.ts
+++ server/server.ts
@@ -12,7 +12,7 @@ app.use(express.json());
 const angularAppPath = path.join(__dirname, 'public/leet-code-stats');
 app.use(express.static(angularAppPath));
 
-app.use('/api/leetcode', leetcodeRoutes);
+app.use('/_api/leetcode', leetcodeRoutes);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(angularAppPath, 'index.html'));
