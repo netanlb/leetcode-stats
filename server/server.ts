5c1c57a1723d1ecb1a0fe7e42480df10cbdc116b
netanlb <netanlb@gmail.com>
2025-01-05 19:42:32 +0200

    deploy

--- server/server.ts
+++ server/server.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import leetcodeRoutes from './routes/leetcode.route';
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
@@ -12,7 +12,7 @@ app.use(express.json());
 const angularAppPath = path.join(__dirname, 'public/leet-code-stats/browser');
 app.use(express.static(angularAppPath));
 
-app.use('/_api/leetcode', leetcodeRoutes);
+app.use('/api/leetcode', leetcodeRoutes);
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(angularAppPath, 'index.html'));
@@ -22,4 +22,4 @@ app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-module.exports = app;
+export default app;
